03ed21ca39faa06aec3705c612926abaf384244c
azinchen <azinchen@student.hive.fi>
2024-12-24 20:51:43 +0200

    learning thread functions

--- philo/include/philosophers.h
+++ philo/include/philosophers.h
@@ -14,6 +14,65 @@
 # define PHILOSOPHERS_H
 
 # include <limits.h>
+# include <string.h> (memset)
+# include <stdio.h> (printf)
+# include <stdlib.h> (malloc, free)
+# include <unistd.h> (write, usleep)
+# include <sys/time.h> (gettimeofday)
+# include <pthread.h> (pthread_create, pthread_detach, pthread_join, pthread_mutex_init)
+
+
+
+/*
+int usleep(useconds_t usec);
+usleep -  stops the execution of the calling thread for a specified number of microseconds
+returns: 0 on success, or -1 on failure
+
+int gettimeofday(struct timeval *tv, struct timezone *tz);
+tv: pointer to a timeval structure to store the current time.
+tz: pointer to a timezone structure (usually NULL).
+gettimeofday - gets the current time and date.
+returns: 0 on success, -1 on error
+
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
+pthread_create - creates a new thread
+thread: pointer to a pthread_t variable to hold the thread ID.
+attr: pointer to a pthread_attr_t structure for thread attributes (or NULL).
+start_routine: function to be executed by the thread.
+arg: argument passed to the start routine.
+returns: 0 on success, error code on failure
+
+int pthread_detach(pthread_t thread);
+pthread_detach detaches (turns off) a thread so that it can release resources automatically when it finishes execution
+returns: 0 on success, error code on failure
+
+int pthread_join(pthread_t thread, void **retval);
+pthread_join - to wait for a thread to terminate and extract its exit status
+thread: thread to join (wait for).
+retval: pointer to a variable where the exit status of the thread will be stored
+returns: 0 on success, error code on failure
+
+int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr);
+pthread_mutex_init initializes a mutex (mutual exclusion) lock
+mutex: The mutex to initialize.
+attr: Mutex attributes (or NULL).
+returns: 0 on success, error code on failure
+
+int pthread_mutex_destroy(pthread_mutex_t *mutex);
+pthread_mutex_destroy destroys a mutex, releasing any resources it uses
+mutex: The mutex to destroy.
+returns: 0 on success, error code on failure
+
+int pthread_mutex_lock(pthread_mutex_t *mutex);
+pthread_mutex_lock locks a mutex, preventing other threads from accessing the shared resource
+mutex: The mutex to lock.
+returns: 0 on success, error code on failure
+
+int pthread_mutex_unlock(pthread_mutex_t *mutex);
+pthread_mutex_unlock unlocks a mutex, allowing other threads to access the shared resource
+mutex: The mutex to unlock.
+returns: 0 on success, error code on failure
+*/
 
 typedef struct s_args
 {
--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -10,10 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/philosophers.h"
+//#include "../include/philosophers.h"
+
+#include <pthread.h>
+#include <stdio.h>
+
+void* some_work(void* arg) 
+{
+    for(int i = 0; i < 5; ++i) 
+    {
+        puts(arg);
+    }
+    return NULL;
+}
+int main(void) 
+{ 
+    pthread_t thread1, thread2;
+    pthread_create(&thread1, NULL, some_work, "Hello World" );
+    pthread_create(&thread2, NULL, some_work, "Hello METANIT.COM" );
+ 
+    printf("End...\n");
+    return 0;
+}
+
+
+/*
+void	*routine(char *str)
+{
+
+}
+
 
 int	main(int argc, char **argv)
 {
 	
 	return (0);
-}
+}*/
+
+/*
+each philosopher is a thread
+each fork is a mutex (mutual exclusion): variable, that can be locked or unlocked
+	mutex protects object from other processes' access to it,
+	bc only one thread can change the data of the object at a time
+
+
+*/
