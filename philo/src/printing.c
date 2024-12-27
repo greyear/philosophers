1276081fe6a50d6611fe00f2a95768d513c53a8c
azinchen <azinchen@student.hive.fi>
2024-12-27 04:13:27 +0200

    minor

--- philo/src/printing.c
+++ philo/src/printing.c
@@ -12,11 +12,15 @@
 
 #include "../include/philosophers.h"
 
-size_t get_time(void)
+size_t get_time_millisec(void) //long?
 {
 	struct timeval tv;
 
-	gettimeofday(&tv, NULL);
+	if (gettimeofday(&tv, NULL) == -1)
+		return (-1);
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+	//sec -> millisec			  microces->millisec
 }
+
+
 
